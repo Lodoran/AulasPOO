6b0ee0328fa18fbdd887d96b7ee3ed36df04308a
Lodoran <63023086+Lodoran@users.noreply.github.com>
2020-04-08 19:11:31 -0300

    Add files via upload

---
+++ primeiro.java
@@ -0,0 +1,13 @@
+import java.until.Scanner;
+public class primeiro{
+public static void main(String[] args){
+Scanner numero = new Scanner (System.in);
+int numero1, numero2, soma;
+System.out.println ("Digite o valor de A: ");
+numero1 = scan.nextInt(); 
+System.out.println ("Digite o valor de B: ");
+numero2 = scan.nextInt();
+soma= numero1 + numero2;
+System.out.println("X= ",+ soma);
+}
+}
